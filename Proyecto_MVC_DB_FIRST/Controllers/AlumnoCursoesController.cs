a6a7820b331c218bf608cd70f04c0752e23d4121
GabrielAlvB <gabrielalvarez010100@gmail.com>
2023-03-31 13:40:15 -0600

    Se agregaron las vistas de alumnos, cursos y alumnoCursos

---
+++ Proyecto_MVC_DB_FIRST/Controllers/AlumnoCursoesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_MVC_DB_FIRST.Data;
+using Proyecto_MVC_DB_FIRST.Models;
+
+namespace Proyecto_MVC_DB_FIRST.Controllers
+{
+    public class AlumnoCursoesController : Controller
+    {
+        private AlumnosContext db = new AlumnosContext();
+
+        // GET: AlumnoCursoes
+        public ActionResult Index()
+        {
+            var alumnoCursoes = db.AlumnoCursoes.Include(a => a.IdAlumnoNavigation).Include(a => a.IdCursoNavigation);
+            return View(alumnoCursoes.ToList());
+        }
+
+        // GET: AlumnoCursoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlumnoCurso alumnoCurso = db.AlumnoCursoes.Find(id);
+            if (alumnoCurso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumnoCurso);
+        }
+
+        // GET: AlumnoCursoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAlumno = new SelectList(db.Alumnos, "IdAlumno", "Nombre");
+            ViewBag.IdCurso = new SelectList(db.Cursos, "IdCurso", "Nombre");
+            return View();
+        }
+
+        // POST: AlumnoCursoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,IdAlumno,IdCurso")] AlumnoCurso alumnoCurso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AlumnoCursoes.Add(alumnoCurso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAlumno = new SelectList(db.Alumnos, "IdAlumno", "Nombre", alumnoCurso.IdAlumno);
+            ViewBag.IdCurso = new SelectList(db.Cursos, "IdCurso", "Nombre", alumnoCurso.IdCurso);
+            return View(alumnoCurso);
+        }
+
+        // GET: AlumnoCursoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlumnoCurso alumnoCurso = db.AlumnoCursoes.Find(id);
+            if (alumnoCurso == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAlumno = new SelectList(db.Alumnos, "IdAlumno", "Nombre", alumnoCurso.IdAlumno);
+            ViewBag.IdCurso = new SelectList(db.Cursos, "IdCurso", "Nombre", alumnoCurso.IdCurso);
+            return View(alumnoCurso);
+        }
+
+        // POST: AlumnoCursoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,IdAlumno,IdCurso")] AlumnoCurso alumnoCurso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(alumnoCurso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAlumno = new SelectList(db.Alumnos, "IdAlumno", "Nombre", alumnoCurso.IdAlumno);
+            ViewBag.IdCurso = new SelectList(db.Cursos, "IdCurso", "Nombre", alumnoCurso.IdCurso);
+            return View(alumnoCurso);
+        }
+
+        // GET: AlumnoCursoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AlumnoCurso alumnoCurso = db.AlumnoCursoes.Find(id);
+            if (alumnoCurso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(alumnoCurso);
+        }
+
+        // POST: AlumnoCursoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            AlumnoCurso alumnoCurso = db.AlumnoCursoes.Find(id);
+            db.AlumnoCursoes.Remove(alumnoCurso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto_MVC_DB_FIRST/Data/AlumnosContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_MVC_DB_FIRST.Data
+{
+    public class AlumnosContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public AlumnosContext() : base("name=AlumnosContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Proyecto_MVC_DB_FIRST.Models.Alumnos> Alumnos { get; set; }
+
+        public System.Data.Entity.DbSet<Proyecto_MVC_DB_FIRST.Models.AlumnoCurso> AlumnoCursoes { get; set; }
+
+        public System.Data.Entity.DbSet<Proyecto_MVC_DB_FIRST.Models.Cursos> Cursos { get; set; }
+    }
+}
